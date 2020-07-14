1ed04d8d3d55a0600e4ad33911bc11787962b154
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-14 02:09:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ end
 
 def find_the_cheese(strings)
   strings.find do |cheese|
-   cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese_types = ["cheddar", "gouda", "camembert"]
  end
 end
