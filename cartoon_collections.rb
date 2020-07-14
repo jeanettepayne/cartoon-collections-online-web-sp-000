9e4ae62497e38cc3eabe7076a524d695439afc98
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-14 02:08:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(strings)
-  strings.include? do |cheese|
+  strings.any? do |cheese|
   cheese_types = ["cheddar", "gouda", "camembert"]
  end
 end
