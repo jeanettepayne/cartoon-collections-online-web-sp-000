bb4285e9260975bcb5244c94970c7d1b92cf59ef
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-14 01:45:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(calls)
-  calls.map do |element|
-    element.
+  calls.map {|element| + "!"
+    
 end
 
 def long_planeteer_calls# code an argument here
